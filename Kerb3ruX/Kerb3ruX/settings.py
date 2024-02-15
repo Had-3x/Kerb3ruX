124f70c2ef9e0d8b14724797837d1a58af17c726
Diego Moncada <moncadadiego999@gmail.com>
2024-02-14 20:54:34 -0500

    new index page at db

--- Kerb3ruX/Kerb3ruX/settings.py
+++ Kerb3ruX/Kerb3ruX/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Kerb3ruX.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['db/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Kerb3ruX/Kerb3ruX/urls.py
+++ Kerb3ruX/Kerb3ruX/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from db.views import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('db/', index),
 ]
