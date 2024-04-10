25d9a2bdb278ff378b4913c5711126be3430ea80
Had-3x <had3x@proton.me>
2024-04-10 17:11:22 -0500

    Commented code

--- Test/Kerb3ruX_Lib.py
+++ Test/Kerb3ruX_Lib.py
@@ -13,23 +13,19 @@ class Errors:
         ...
 
 def getCode(Data) -> str:
-        if isinstance(Data, str):
-            return "0x001"
-        elif isinstance(Data, dict):
-            return "0x002"
-        else:
-            raise Errors.UnknowType()
+    ''' 
+    Returns the transmission code based on the type of data provided. (see: Transmision Codes Table).
+    '''
+    if isinstance(Data, str):
+        return "0x001"
+    elif isinstance(Data, dict):
+        return "0x002"
+    else:
+        raise Errors.UnknowType()
 
 class Security:
 
     def __init__(self, keySize: int = 1024) -> None:
-        '''
-        Is used to generate a public and private key pair for RSA cryptography.\n
-        Parameters:
-        - keySize(int): Is the size of the key in bits. By default, 2048.\n
-        Return:
-        - A dictionary containing both, the public key(publicKey) and the private key(privateKey).
-        '''
         self.privateKey = rsa.generate_private_key(65537, keySize)
         self.publicBytes = self.privateKey.public_key().public_bytes(encoding=serialization.Encoding.PEM,
             format=serialization.PublicFormat.SubjectPublicKeyInfo)
@@ -37,12 +33,12 @@ class Security:
     
     def Encrypt(self, Data: bytes, remoteKey: PublicKeyTypes) -> bytes:
         '''
-        Is used to encrypt data using an RSA public key.\n
+        Is used to encrypt data using an RSA public key.
         Parameters:
-        - Data(bytes): The text to be encrypted\n
-        - remoteKey(RSAPublicKey): The public key with which the data is to be encrypted. This key needs to be loaded before use (See Load_PEM)\n
+        - Data(bytes): The text to be encrypted
+        - remoteKey(RSAPublicKey): The public key with which the data is to be encrypted. This key needs to be loaded before use (See Load_PEM)
         Return:
-        - Encrypted data in bytes
+        - Bytes -> Encrypted data
         '''
         if isinstance(remoteKey, rsa.RSAPublicKey):
             text = remoteKey.encrypt(
@@ -58,11 +54,11 @@ class Security:
 
     def Decrypt(self, Data: bytes, Encode: str ='utf-8') -> str:
         '''
-        Is used to decrypt data that has been encrypted with an RSA public key.\n
+        Is used to decrypt data that has been encrypted with an RSA public key.
         Parameters:
         - Data(bytes): The set of bytes to be decrypted.
         - privateKey(RSAprivateKey): The private key with which the data is to be decrypted.
-        - Encode(str): Decoding is performed using the encoding specified. By default is 'utf-8'.\n
+        - Encode(str): Decoding is performed using the encoding specified. By default is 'utf-8'.
         Return:
         - A text string with decrypted data.
         '''
@@ -77,63 +73,84 @@ class Security:
         return text.decode(Encode)
 
     def Hash(self, Data: bytes) -> str:
+        '''
+        Calculate the Hash of the provided data.
+        Parameters:
+        - Data(Bytes): The data to be Hashed
+        Return:
+        - Str -> Hash
+        '''
         digest = hashlib.sha256()
         digest.update(Data)
         return digest.hexdigest()
 
     def LoadKey(self, remoteKey: bytes) -> PublicKeyTypes:
+        '''
+        Loads the bytes of a remote public key for its use.
+        Parameters:
+        - remoteKey(bytes): Public key in bytes.
+        Return:
+        - PublicKeyTypes -> Public key for use.
+        '''
         return serialization.load_pem_public_key(remoteKey)
 
 class Server:
     def __init__(self):
         logging.basicConfig(filename=f'{datetime.date.today()}.log', filemode='a', level=logging.DEBUG,
                             format='> [%(threadName)s] [%(asctime)s] [%(levelname)s]\n \t%(message)s',
-                            datefmt='%I:%M:%S %p')
-        self.executor = ThreadPoolExecutor(max_workers=5)
-        self.clientAddrs = []
+                            datefmt='%I:%M:%S %p') # Configure how logs will be stored 
+        self.executor = ThreadPoolExecutor(max_workers=5) # Create a Thread Pool to manage processes
+        self.clientAddrs = [] # List to storage slave's ip
 
-    def Listen(self, host='localhost', port=4433):
-        listenerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def Listen(self, host: str = 'localhost', port: int = 4433):
+        '''
+        Listen for incoming connections on the specified HOST and PORT (default localhost:4433).
+        Parameters:
+        - host(str): Ip or Domain to listen.
+        - port(int): Port to listen.
+        Return:
+        - NULL
+        '''
+        listenerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create Socket
         listenerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        listenerSocket.bind((host, port))
-        listenerSocket.listen()
-        logging.info(f"Server listening on {host}:{port}")
+        listenerSocket.bind((host, port)) # Assign the socket to the host and port
+        listenerSocket.listen() # Listen for connections
+        logging.info(f"Server listening on {host}:{port}") # Log listening
         
         while True:
-            remoteSocket, remoteAddr = listenerSocket.accept()
+            remoteSocket, remoteAddr = listenerSocket.accept() # Accept Connections
             #client_sockets.append(remoteSocket) Save To DB
             #client_addresses.append(remoteAddr) Save To DB
-            logging.info(f"New Conecction from {remoteAddr}")
-            self.clientAddrs.append(remoteAddr)
-            self.executor.submit(self.Handle_client, remoteSocket, remoteAddr)
+            logging.info(f"New Conecction from {remoteAddr}") # Log new connection
+            self.clientAddrs.append(remoteAddr) # Save connection address
+            self.executor.submit(self.Handle_client, remoteSocket, remoteAddr) # Create thread for connection
 
     def Handle_client(self, client_socket: socket.SocketType, remoteAddr: str):
         try:
             while True:
-                RawData = client_socket.recv(2048)
+                RawData = client_socket.recv(2048) # Receive data
                 if not RawData:
-                    break
-                #Enviar Datos
-                Data = re.findall("<*>", str(RawData))
-                if (Data[0] == "2x000"):
-                    if(Data[1] == remoteAddr):
+                    break # If recieved data is empty, close the connection
+                Data = re.findall(r'(?<!\\)<.*?(?<!\\)>', str(RawData)) # Divide data into fields separated by <>.
+                if (Data[0] == "2x000"): # Data is a new connection request
+                    if(Data[1] == remoteAddr): # check if the sender's ip matches the declared ip
                         ...
-                logging.info(f"Recived Data From {remoteAddr}")
+                logging.info(f"Recived Data From {remoteAddr}") # Log data
         except Exception as e:
             logging.warn(f"Error handling client: {e}")
         finally:
             client_socket.close()
 
-    def Send(self, remoteAddr: str, data: bytes):
-        if remoteAddr in self.clientAddrs:
-            senderSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def Connect(self, remoteAddr: str, data: bytes):
+        if remoteAddr in self.clientAddrs: # Check for known ip
+            senderSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create Socket
             senderSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             try:
-                senderSocket.connect(remoteAddr)
+                senderSocket.connect(remoteAddr) # Try connection
             except ConnectionRefusedError as exception:
-                self.clientAddrs.remove(remoteAddr)
+                self.clientAddrs.remove(remoteAddr) # Remove dead slave
             else:
-                self.executor.submit(senderSocket.sendall, data)
+                self.executor.submit(senderSocket.sendall, data) # Create thread for connection
             finally:
                 senderSocket.close()
         else:
@@ -141,25 +158,23 @@ class Server:
 
 class Client:
     def __init__(self, remoteInfo: tuple[str | None, int], localInfo: tuple[str, str, int, int]):
-        self.remoteAddr = remoteInfo
-        self.security = Security()
-        self.name = localInfo[0] #Program Name, Generated By Server
-        self.passphrase = localInfo[1] #Generated by Server, NO CLIENT, ENCRYPTED
-        self.bufSize = localInfo[2]
-        self.port = localInfo[3]
-        self.ip = get('https://checkip.amazonaws.com').text.strip()
+        self.remoteAddr = remoteInfo # Server data for sockets
+        self.security = Security() # Create an instance of security class
+        self.name = localInfo[0] # Program Name, Generated By Server
+        self.passphrase = localInfo[1] # Generated by Server, NO CLIENT, ENCRYPTED
+        self.bufSize = localInfo[2] # Buffer Size for sockets
+        self.port = localInfo[3] # Local port to use
+        self.ip = get('https://checkip.amazonaws.com').text.strip() # Public Ip for checks
         
     def Connect(self, Data):
         try:
-            sock = socket.create_connection(self.remoteAddr, timeout=30)
+            sock = socket.create_connection(self.remoteAddr, timeout=30) # Connect to server
         except ConnectionRefusedError as error:
             ...
         else:
-            content = f"<{getCode(Data)}><{str(Data).encode()}>".encode()
-            header = f"<2x000><{self.ip}><{self.name}><{self.passphrase}><{getsizeof(content)}>".encode()
-            tail = f"<0x005><{self.security.Hash(str(Data).encode())}>".encode()
-            
-            
+            content = f"<{getCode(Data)}><{str(Data).encode()}>".encode() # Data to send
+            header = f"<2x000><{self.ip}><{self.name}><{self.passphrase}><{getsizeof(content)}>".encode() # Header of connection
+            tail = f"<0x005><{self.security.Hash(str(Data).encode())}>".encode() # Tail of connection
         finally:
             sock.close()
 
